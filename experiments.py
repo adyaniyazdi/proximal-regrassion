965e8e66c4a1c6116d158202d519b9dbd10668ee
hudginspj <hudginspj@mymail.vcu.edu>
2017-11-18 19:51:22 -0500

    Added noise, 1st-degree convergence if noise > 8

--- experiments.py
+++ experiments.py
@@ -1,8 +1,14 @@
+import random
 
 import proxregression as pr
 import numpy as np
 
 print("import done")
+
+def banner(msg):
+    print("=" * 75)
+    print(msg)
+    print("=" * 65)
 
 
 def run_experiment(params, generate_beta):
@@ -29,6 +35,22 @@ def gen_alternating_support_beta(params, groups):
         real_beta[i*2] = 0.0
     return real_beta
 
+def gen_uniform_beta(params, groups):
+    return np.random.normal(0, 1, params.num_features)
+
+def gen_one_group_zero_beta(params, groups):
+    real_beta = np.random.normal(0, 1, params.num_features)
+    for j in groups[0]:
+        real_beta[j] = 0.0
+    return real_beta
+
+def gen_one_group_supported_beta(params, groups):
+    real_beta = np.zeros(params.num_features)
+    for j in groups[0]:
+        real_beta[j] = random.gauss(0, 1)
+    return real_beta
+
+
 params = pr.Parameters()
 params.num_examples = 500 #N
 params.num_groups = 10
@@ -38,13 +60,19 @@ params.group_overlap = 3
 # (x, y, real_beta, groups) = pr.generate_sample_data(params)
 
 repetitions = 2
-print("="*25)
-print("half support experiments:")
-print("="*25)
+banner("half support experiments:")
 for i in range(repetitions):
     run_experiment(params, gen_half_support_beta)
-print("="*25)
-print("alternating support experiments:")
-print("="*25)
+banner("alternating support experiments:")
 for i in range(repetitions):
     run_experiment(params, gen_alternating_support_beta)
+# banner("uniform experiments:")
+# for i in range(repetitions):
+#     run_experiment(params, gen_uniform_beta)
+banner("one-group-zero experiments:")
+for i in range(repetitions):
+    run_experiment(params, gen_one_group_zero_beta)
+# banner("one-group-supported experiments:")
+# for i in range(repetitions):
+#     run_experiment(params, gen_one_group_supported_beta)
+
--- proxregression.py
+++ proxregression.py
@@ -25,153 +25,171 @@ def generate_groups(params):
     return groups
 
 def generate_training_data(beta, params):
+    error_variance = 0.8 #TODO tune
     x = np.random.normal(0, 1, (params.num_examples, params.num_features))
     # print("beta",beta)
-    y = np.matmul(x,beta) #TODO add epsilon
+    y = np.matmul(x,beta) + np.random.normal(0, error_variance, params.num_examples)
 
     return x, y
 
 
+def test(learned_beta, real_beta, params):
+    test_x = np.random.normal(0, 1, (params.num_examples, params.num_features))
+    actual_y = np.matmul(test_x, real_beta)  # TODO add epsilon
+    predicted_y = np.matmul(test_x, learned_beta)
+    errors = np.subtract(actual_y, predicted_y)
+    avg_error = np.sum(np.absolute(errors)) / params.num_examples #TODO use reduce and l2
+    # print("avg_error", avg_error)
+    return avg_error
+
+###########################################################
+# Learning algorithm
+
+
+# From top of page 4
+def group_weight(group):
+    return math.sqrt(group.__len__())
+
+
+# From equation 4
+def build_c(groups, sparsity_param, num_features):
+    c_height = 0
+    for group in groups:
+        c_height += group.__len__()
+    c = np.zeros((c_height, num_features))
+    i_g_pair = 0
+    for group in groups:
+        w = group_weight(group)
+        for j in group:
+            c[i_g_pair, j] = sparsity_param * w
+            i_g_pair += 1
+    return c
+
+
+# From equation 8
+def abs_gamma(groups, num_examples, sparsity_param): #TODO switch inner and outer loops to improve time complexity
+    weights_norms = [] #TODO figure out how to describe this
+    for j in range(num_examples):
+        sum_squares = 0.0
+        for group in groups:
+            if j in group:
+                sum_squares += math.pow(group_weight(group),2)
+        weights_norms.append(math.sqrt(sum_squares))
+    # print(weight_totals)
+    return sparsity_param * max(weights_norms)
+
+# print("abs_gamma", abs_gamma(groups, params.num_examples, sparsity_param=sparsity_param))
+
+
+# From equation 10
+def lipschitz_constant(x, groups, num_examples, sparsity_param, mu):
+    xt_x = np.matmul(np.transpose(x),x)
+    eigens = np.linalg.eig(xt_x)[0].tolist()
+    eigens = [elem.real for elem in eigens if elem.imag == 0.0]  #TODO confirm max real
+    #print("eigens", eigens)
+    max_eigen = max(eigens)
+    #print("max_eigen", max_eigen)
+    abs_g = abs_gamma(groups, num_examples, sparsity_param)
+
+    return max_eigen + (math.pow(abs_g, 2) / mu) #TODO not sure what's going on with the power, seems to work better with 1
+
+
+def shrinkage(arr):
+    norm = np.linalg.norm(arr, ord=2) #TODO Make sure correct ord
+    if norm > 1.0:
+        return arr/norm
+    else:
+        return arr
+
+#from lemma 1
+def gen_opt_alpha(beta, groups, sparsity_param, mu):
+    alpha_gs = []
+    for group in groups:
+        beta_g = []
+        for j in group:
+            beta_g.append(beta[j])
+        bg = np.array(beta_g)
+        ag = bg * sparsity_param * group_weight(group) / mu
+
+        #print("b/a", beta_g, shrinkage(ag))
+        # print("norm alpha", np.linalg.norm(np.array(alpha_g)))
+        alpha_gs += shrinkage(ag).tolist()
+    total_opt_alpha = np.array(alpha_gs)
+    # print("alpha_star", alpha_gs)
+    return total_opt_alpha  # vector with length = sum of group sizes, same as height of C
+
+# print("opt alpha", gen_opt_alpha(b, groups, sparsity_param))
+
+
+# Equation 9
+def f_squiggle_gradient(x, y, b, c, groups, sparsity_param, mu):
+    opt_alpha = gen_opt_alpha(b, groups, sparsity_param, mu)
+    term2 = np.matmul(np.transpose(c), opt_alpha) #TODO check dimensions
+    term1 = np.matmul(np.transpose(x), ((np.matmul(x, b)) - y))
+    return term1 + term2
+
+
+def test_convergence(t, betas_t, weights_t, z_t, gradient_t):
+    if (t<2):
+        return False
+    change_arr = np.absolute(np.subtract(betas_t[t], betas_t[t - 1]))
+    change_in_beta = np.sum(change_arr)
+    prior_change_in_beta = np.sum(np.absolute(np.subtract(betas_t[t-1], betas_t[t - 2])))
+    max_change = 0.0
+    max_change_index = 0
+    beta = betas_t[t]
+    for i in range(beta.shape[0]):
+        if change_arr[i] > max_change:
+            max_change = change_arr[i]
+            max_change_index = i
+
+    if t % 100 == 0:
+        i = max_change_index
+        print("t", t, "change", change_in_beta, "max_c", max_change, "i", i,
+              "mc_beta", betas_t[t][i], "mc_w", weights_t[t][i], "mc_z", z_t[t][i], "mc_gr", gradient_t[t][i])
+
+    if abs(change_in_beta - prior_change_in_beta) < 0.0000001 and change_in_beta > 0.01:
+        print("!!! Convergence due to 2nd-degree change in beta")
+        return True
+    if change_in_beta < 0.0001:
+        print("!!! Convergence due to 1st-degree change in beta")
+        return True
+    return False
+
+
 def learn(x, y, groups, params, sparsity_param, desired_accuracy):
-    # From top of page 4
+    c = build_c(groups, 5.0, params.num_features)
+    # print("c", c)
     mu = desired_accuracy / groups.__len__()
 
-    def group_weight(group):
-        return math.sqrt(group.__len__())
-
-    #From equation 4
-    def build_c(groups, sparsity_param, num_features):
-        c_height = 0
-        for group in groups:
-            c_height += group.__len__()
-        c = np.zeros((c_height, num_features))
-        i = 0
-        for group in groups:
-            w = group_weight(group)
-            for member in group:
-                c[i, member] = sparsity_param * w
-                i += 1
-        return c
-
-    c = build_c(groups, 5.0, params.num_features)
-    #print("c", c)
-
-
-    # From equation 8
-    def abs_gamma(groups, num_examples, sparsity_param): #TODO switch inner and outer loops to improve time complexity
-        weight_totals = [] #TODO figure out how to describe this
-        for j in range(num_examples):
-            sum_squares = 0.0
-            for group in groups:
-                if j in group:
-                    sum_squares += math.pow(group_weight(group),2)
-            weight_totals.append(math.sqrt(sum_squares))
-        # print(weight_totals)
-        return sparsity_param * max(weight_totals)
-
-    # print("abs_gamma", abs_gamma(groups, params.num_examples, sparsity_param=sparsity_param))
-
-
-    #From equation 10
-    def lipschitz_constant(x, groups, num_examples, sparsity_param):
-        xt_x = np.matmul(np.transpose(x),x)
-        eigens = np.linalg.eig(xt_x)[0].tolist()
-        eigens = [elem.real for elem in eigens if elem.imag == 0.0]
-        #print("eigens", eigens)
-        max_eigen = max(eigens)
-        #print("max_eigen", max_eigen)
-        abs_g = abs_gamma(groups, num_examples, sparsity_param)
-
-        return max_eigen + (abs_g / mu)
-
-    lip_constant = lipschitz_constant(x, groups, params.num_examples, sparsity_param)
+    lip_constant = lipschitz_constant(x, groups, params.num_examples, sparsity_param, mu)
     # print("lip constant", lip_constant)
-
-    w0 = np.zeros(params.num_features)
-
-
-
-    def shrinkage(arr):
-        norm = np.linalg.norm(arr)
-        if norm > 1.0:
-            return arr/norm
-        else:
-            return arr
-    #from lemma 1
-    def gen_opt_alpha(beta, groups, sparsity_param):
-        alpha_gs = []
-        for group in groups:
-            beta_g = []
-            for j in group:
-                beta_g.append(beta[j])
-            bg = np.array(beta_g)
-            ag = bg * sparsity_param * group_weight(group) / mu
-
-            #print("b/a", beta_g, shrinkage(ag))
-            # print("norm alpha", np.linalg.norm(np.array(alpha_g)))
-            alpha_gs += shrinkage(ag).tolist()
-        return np.array(alpha_gs)
-
-    # print("opt alpha", gen_opt_alpha(b, groups, sparsity_param))
-
-
-    #Equation 9
-    def f_squiggle_gradient(x, y, b, groups, sparsity_param):
-        opt_alpha = gen_opt_alpha(b, groups, sparsity_param)
-        term2 = np.matmul(np.transpose(c), opt_alpha) #TODO check dimensions
-        term1 = np.matmul(np.transpose(x), ((np.matmul(x, b)) - y))
-        return term1 + term2
-
-
-    def testConvergence(t, betas_t):
-        if (t<2):
-            return False
-        change_arr = np.absolute(np.subtract(betas_t[t], betas_t[t - 1]))
-        change_in_beta = np.sum(change_arr)
-        prior_change_in_beta = np.sum(np.absolute(np.subtract(betas_t[t-1], betas_t[t - 2])))
-        max_change = 0.0
-        max_change_index = 0
-        beta = betas_t[t]
-        for i in range(beta.shape[0]):
-            if change_arr[i] > max_change:
-                max_change = change_arr[i]
-                max_change_index = i
-
-
-        if (t % 1 == 0):
-            print("t", t, "change", change_in_beta, "max_c", max_change, "i", max_change_index, "mc_val", betas_t[t][max_change_index])
-        if abs(change_in_beta - prior_change_in_beta) < 0.00001: #and change_in_beta > 0.1:
-            print("!!! Convergence due to 2nd-degree change in beta")
-            return True
-        if change_in_beta < 0.0001:
-            print("!!! Convergence due to 1st-degree change in beta")
-            return True
-        return False
-
-    # Algorithm
 
     gradient_t = []
     weights_t = []
     beta_t = []
     z_t = []
     weights_t.append(np.zeros(params.num_features))
+    #beta_t.append(np.zeros(params.num_features)) #TODO delete
 
     start = datetime.datetime.now()
     for t in range(1000):
         #step 1
-        gradient_t.append(f_squiggle_gradient(x, y, weights_t[t], groups, sparsity_param))
+        gradient_t.append(f_squiggle_gradient(x, y, weights_t[t], c, groups, sparsity_param, mu))
+        #gradient_t.append(f_squiggle_gradient(x, y, beta_t[t], groups, sparsity_param, mu)) #TODO delete
         #step 2
         #print("gradient/lip_constant", gradient, lip_constant, gradient/lip_constant)
-        beta_t.append(weights_t[t] - (gradient_t[t]/lip_constant))
+        beta_t.append(weights_t[t] - (gradient_t[t]/(lip_constant))) # TODO tried dividing by 2 here
+        #beta_t.append(beta_t[t] - (gradient_t[t] / (lip_constant))) #TODO delete
         #step 3
-        z = 0 - ((t + 1) * gradient_t[t] / 2) / lip_constant
+        z = -((t + 1) * gradient_t[t] / 2) / lip_constant
         if (t > 0): z += z_t[t-1]
         z_t.append(z)
         #print("z/lip_constant", z, lip_constant, z / lip_constant)
         #step 4
-        weights = ((t+1)*beta_t[t] / (t+3)) + (2*beta_t[t] / (t+3))
+        weights = ((t+1)*beta_t[t] / (t+3)) + (2*z_t[t] / (t+3))
         weights_t.append(weights)
-        if testConvergence(t, beta_t):
+        if test_convergence(t, beta_t, weights_t, z_t, gradient_t):
             break
 
 
@@ -191,14 +209,7 @@ def learn(x, y, groups, params, sparsity_param, desired_accuracy):
 # print("b", b)
 
 
-def test(learned_beta, real_beta, params):
-    test_x = np.random.normal(0, 1, (params.num_examples, params.num_features))
-    actual_y = np.matmul(test_x, real_beta)  # TODO add epsilon
-    predicted_y = np.matmul(test_x, learned_beta)
-    errors = np.subtract(actual_y, predicted_y)
-    avg_error = np.sum(np.absolute(errors)) / params.num_examples #TODO use reduce and l2
-    # print("avg_error", avg_error)
-    return avg_error
+
 
 
 
